26fcef848d4d0b68179f826f92f2303450ad8836
Filip Strandberg <filip.strandberg@hyph.com>
2025-04-19 14:29:29 +0200

    remove express

--- functions/index.js
+++ functions/index.js
@@ -1,14 +1,10 @@
 import { InteractionResponseType, InteractionType, verifyKeyMiddleware } from 'discord-interactions';
 import 'dotenv/config';
-import express from 'express';
 import { initializeApp } from 'firebase-admin/app';
 import { setGlobalOptions } from 'firebase-functions';
 import { onRequest } from 'firebase-functions/https';
 import { handleCommands } from '../src/handle-commands.js';
 import { handleInteractions } from '../src/handle-interactions.js';
-
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
 
 initializeApp();
 setGlobalOptions({ region: 'europe-west3' });
@@ -16,20 +12,17 @@ setGlobalOptions({ region: 'europe-west3' });
 // To keep track of our active games
 const activeGames = {};
 
-const app = express();
-
 console.log('process.env.PUBLIC_KEY: ', process.env.PUBLIC_KEY);
-
-// app.use(verifyKeyMiddleware(process.env.PUBLIC_KEY));
 
 /**
  * Interactions endpoint URL where Discord will send HTTP requests
  * Parse request body and verifies incoming requests using discord-interactions package
  */
-app.post('/', (req, res) => {
-  const { type } = req.body;
-
+export const discord = onRequest(async (req, res) => {
+  // Use discord-interactions to verify the request
   verifyKeyMiddleware(process.env.PUBLIC_KEY);
+
+  const { type } = req.body;
 
   switch (type) {
     case InteractionType.PING:
@@ -43,5 +36,3 @@ app.post('/', (req, res) => {
       return res.status(400).json({ error: 'unknown interaction type' });
   }
 });
-
-export const discord = onRequest(app);
