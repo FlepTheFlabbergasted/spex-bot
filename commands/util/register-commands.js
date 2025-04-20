0baa840f5d779dff1ff4dc44beb7dcefce9af139
Filip Strandberg <filip.strandberg@hyph.com>
2025-04-20 15:06:51 +0200

    deployment to google app engine works

--- commands/util/register-commands.js
+++ commands/util/register-commands.js
@@ -1,6 +1,16 @@
 import { REST, Routes } from 'discord.js';
-import 'dotenv/config';
+import dotenv from 'dotenv';
+import { dirname, join } from 'node:path';
+import { fileURLToPath } from 'node:url';
 import { ALL_COMMANDS } from '../index.js';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+dotenv.config({ path: join(__dirname, '../../.env') });
+
+const APP_ID = process.env.APP_ID;
+const DISCORD_TOKEN = process.env.DISCORD_TOKEN;
+const GUILD_DEV_ID = process.env.GUILD_DEV_ID;
+const NODE_ENV = process.env.NODE_ENV;
 
 const commandData = [];
 
@@ -13,14 +23,17 @@ for (const command of ALL_COMMANDS) {
 }
 
 // Construct and prepare an instance of the REST module
-const rest = new REST().setToken(token);
+const rest = new REST().setToken(DISCORD_TOKEN);
 
 (async () => {
   try {
     console.log(`Started refreshing ${commandData.length} application (/) commands.`);
 
     // The put method is used to fully refresh all commands in the guild with the current set
-    const data = await rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commandData });
+    const data = await rest.put(
+      NODE_ENV === 'prod' ? Routes.applicationCommands(APP_ID) : Routes.applicationGuildCommands(APP_ID, GUILD_DEV_ID),
+      { body: commandData }
+    );
 
     console.log(`Successfully reloaded ${data.length} application (/) commands.`);
   } catch (error) {
